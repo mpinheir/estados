ebfb3c8b542d7716c8636b0d295b2639bda6eb20
Marcelo Pinheiro <mpinheir@gmail.com>
2019-11-26 20:50:02 -0800

    Melhoria das mensagens exibidas no log.

--- estados.go
+++ estados.go
@@ -66,7 +66,10 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-		log.Printf("Defaulting to port %s", port)
+		log.Printf("Assumindo porta %s comp padrão", port)
+	} else {
+		log.Printf("Utilizando porta %s", port)
 	}
+
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
